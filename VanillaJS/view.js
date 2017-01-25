e1bf36d2ea069d620d2a68daae6926b8335d0f0e
Vi Vo <vi.uyen.vo@gmail.com>
2017-01-25 14:18:53 -0800

    move todos to view storage

--- VanillaJS/view.js
+++ VanillaJS/view.js
@@ -1,8 +1,9 @@
-var View = function() {};
+var View = function() {
+  this.todos = document.getElementById('todos');
+};
 
 View.prototype.displayStoredTodos = function() {
   for (var key in model.storage) {
-    var todos = document.getElementById('todos');
     var todo = document.createElement('li');
     todo.textContent = key;
     todos.appendChild(todo);
